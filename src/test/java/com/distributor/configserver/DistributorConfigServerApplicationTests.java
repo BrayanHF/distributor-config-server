7ce5c93c08d355fea4d5450c8ba990b66a1b6e98
Brayan Hernandez <brayanhernandezfurnieles@gmail.com>
2024-02-20 14:10:18 -0500

    Initial commit

---
+++ src/test/java/com/distributor/configserver/DistributorConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributor.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributorConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
